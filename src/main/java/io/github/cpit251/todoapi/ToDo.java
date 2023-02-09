58004666f7d97e323659465adc05fe6e663c8361
yousef <yousef-1122@hotmail.com>
2023-02-09 18:24:02 +0300

    inital

---
+++ src/main/java/io/github/cpit251/todoapi/ToDo.java
@@ -0,0 +1,43 @@
+package io.github.cpit251.todoapi;
+import java.util.UUID;
+
+public class ToDo {
+    private String name;
+    private String id;
+    private boolean isComplete;
+
+    public ToDo(){
+        this.isComplete = false;
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public ToDo(String name) {
+        this.name = name;
+        this.isComplete = false;
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isComplete() {
+        return this.isComplete;
+    }
+
+    public void setComplete(boolean isComplete) {
+        this.isComplete = isComplete;
+    }
+
+    public String toString() {
+        return "ToDo: id: " + this.id + "\nName: " + this.name + "\nDone: " + (this.isComplete ? "Yes" : "No");
+    }
+}
